943a8c214474fad98001977a71eb5c6afceb6aff
Nicolai Poletto <nicolai.poletto@cwi.com.br>
2023-01-20 09:51:16 -0300

    Initial commit

---
+++ src/main/java/com/example/springwolfamqptest/SpringWolfAmqpTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwolfamqptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWolfAmqpTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWolfAmqpTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springwolfamqptest/SpringWolfAmqpTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwolfamqptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWolfAmqpTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
